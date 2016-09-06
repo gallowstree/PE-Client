97ce4f678cd899e3319d55dfa995067714246f50
Alejandro <alex.at019@gmail.com>
2016-09-05 22:17:26 -0600

    Stop listeing after game over

--- Game.cpp
+++ Game.cpp
@@ -209,7 +209,7 @@ void Game::processServerEvents()
 
 void Game::receiveMessage(char buffer[], size_t nBytes, sockaddr_in* serverAddr)
 {
-    if (nBytes <= 0)
+    if (nBytes <= 0 || gameOver)
         return;
 
     int32_t  msgNum;
